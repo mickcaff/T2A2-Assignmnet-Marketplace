e8da418a3721cece6d9393564b4d5bcb8d98124d
mickcaff <mcaffery@outlook.com>
2022-05-13 22:48:05 +1000

    CRUD Actions & Views for Provider/Services

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
+# FarmPlace - Farm Services Marketplace
+
+### What is FarmPlace?
+Farmplace is a two sided marketplace for farmers and agricultural service providers. 
+
+
+### Functionality & Features
+
+
+### Sitemap
+
+
+### Screenshots
+
+
+### Target Audience
+
+
+### Tech Stack
+- Rails
+- testing
+- Stimulus.js - Rails javascript library
+
+### User Stories
+
+
+### Wireframes
+
+
+### High Level Components
+
+
+### Third Party Services
+
+
+### Entity Relationship Diagram
+
+
+### Project Models
+
+
+### Database Relations
+
+
+### Database Schema Design
+
+
+### Trello
+
+
+## GitHub Repo
+
+
+
 # README
 
 This README would normally document whatever steps are necessary to get the
--- app/controllers/provider/services_controller.rb
+++ app/controllers/provider/services_controller.rb
@@ -1,13 +1,65 @@
 class Provider::ServicesController < ApplicationController
+  
   def new
+    @service = Service.new
   end
 
+  def create
+    begin
+      @service = current_user.services.new(service_params)
+      @service.save!
+      redirect_to provider_service_path(@service)
+    rescue
+      flash.now[:alert] = @service.errors.full_messages.join('<br>')
+      render 'new'
+    end
+  end
+  
   def edit
+    @service = current_user.services.find(params[:id])
   end
 
+  def update
+    begin
+      @service = current_user.services.find(params[:id])
+      @service.update!(service_params)
+      redirect_to provider_service_path(@service)
+    rescue
+      flash.now[:alert] = @service.errors.full_messages.join('<br>')
+      render 'edit'
+    end
+  end
+  
   def index
+    @services = current_user.services.all
   end
 
   def show
+    @service = current_user.services.find(params[:id])
   end
+
+  def destroy
+    @service = current_user.services.find(params[:id])
+    @service.update(status: :archived)
+    redirect_to provider_services_path
+  end
+
+  private
+
+  def service_params
+    return params.require(:service).permit(
+      :title,   
+      :about,
+      :address_line1,
+      :address_line2,
+      :city,
+      :state,
+      :postal_code,
+      :country, 
+      :price, 
+      :price_unit, 
+      :additional_charges
+    )
+  end
+
 end
