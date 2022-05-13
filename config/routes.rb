15fbd7b642e6d89b5f6f51e35b1b50ab98ea3dea
mickcaff <mcaffery@outlook.com>
2022-05-13 22:05:41 +1000

    Devise User model added - and rails 7 betterprogramming fix done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+
+  devise_for :users
+  
   root to: "base_pages#home"
 
 end
