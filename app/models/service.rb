bd7d0bf5add75c68247c5ac74ee73ca4114d531f
mickcaff <mcaffery@outlook.com>
2022-05-13 22:24:52 +1000

    Set up model relationship between service and provider

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ApplicationRecord
-  belongs_to :provider
+  belongs_to :provider, class_name: 'User'
+
+  validates :title, presence: true
+  
+  enum :status, [:draft, :published, :archived]
 end
