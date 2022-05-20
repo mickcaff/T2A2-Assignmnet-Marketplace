bd0f1b774af201605be14747ea067cc921ece106
mickcaff <mcaffery@outlook.com>
2022-05-20 11:43:56 +1000

    Heroku deployment working correctly, uploading photos in seeds file is set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Service.destroy_all
+
+admin = User.create(email: 'admin@farm.com', password: 'password')
+user_a = User.create(email: 'a@farm.com', password: 'password')
+user_b = User.create(email: 'b@farm.com', password: 'password')
+user_c = User.create(email: 'c@farm.com', password: 'password')
+user_d = User.create(email: 'd@farm.com', password: 'password')
+
+Service.create(
+    provider_id: user_a.id,
+    title: "Grain Harvesting", 
+    about: "We have three grain harvesters available throughout NSW.", address_line1: "185 Morgan St", 
+    city: "Wagga Wagga", 
+    state: "NSW", 
+    postal_code: "2650", 
+    country: "Australia", 
+    lat: -35.114021, 
+    lng: 147.359726, 
+    price: 600, 
+    price_unit: "per hr", 
+    additional_charges: "none",
+).cover_image.attach(io: File.open("app/assets/images/seed_images/grainharvester1.jpg"), filename: "grainharvester1.jpg", content_type: "image/jpg")
+
+ 
