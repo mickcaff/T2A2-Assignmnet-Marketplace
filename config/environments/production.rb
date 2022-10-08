e5e9a9d1c9e78a1fdf9fb547419a1f6d7ff6a4dd
mickcaff <mcaffery@outlook.com>
2022-10-08 15:36:29 +1000

    followed S.Smith discord instructions for railway deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Added from S.Smith instructions on Discord for Railway deployment
+  config.hosts << "https://t2a2-assignmnet-marketplace-production.up.railway.app/"
 end
